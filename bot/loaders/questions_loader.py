958ad102de38807885c02297480ec3d7b80aa0ed
walking_fires <zqop2013@gmail.com>
2024-11-22 21:22:33 +0300

    bot base update

--- bot/loaders/questions_loader.py
+++ bot/loaders/questions_loader.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from bot.dao.session_maker import connection
-from bot.dao.dao import SymptomQuestionDAO
-from bot.schemas import SymptomQuestionModel
+from bot.users.dao import SymptomQuestionDAO
+from bot.users.schemas import SymptomQuestionModel
 
 
 @connection(commit=True)
@@ -18,10 +18,25 @@ async def load_questions(session: AsyncSession, instances: List[BaseModel]) -> N
 
 if __name__ == '__main__':
     questions = [
-        SymptomQuestionModel(id=1, question='Вопрос 1', question_subject='Субъект 1', answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
-        SymptomQuestionModel(id=2, question='Вопрос 2', question_subject='Субъект 2', answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
-        SymptomQuestionModel(id=3, question='Вопрос 3', question_subject='Субъект 3', answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
-        SymptomQuestionModel(id=4, question='Вопрос 4', question_subject='Субъект 4', answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
-        SymptomQuestionModel(id=5, question='Вопрос 5', question_subject='Субъект 5', answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
+        SymptomQuestionModel(id=1,
+                             question='Вопрос 1',
+                             question_subject='Субъект 1',
+                             answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
+        SymptomQuestionModel(id=2,
+                             question='Вопрос 2',
+                             question_subject='Субъект 2',
+                             answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
+        SymptomQuestionModel(id=3,
+                             question='Вопрос 3',
+                             question_subject='Субъект 3',
+                             answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
+        SymptomQuestionModel(id=4,
+                             question='Вопрос 4',
+                             question_subject='Субъект 4',
+                             answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
+        SymptomQuestionModel(id=5,
+                             question='Вопрос 5',
+                             question_subject='Субъект 5',
+                             answers=['Ответ 1', 'Ответ 2', 'Ответ 3', 'Ответ 4']),
     ]
     asyncio.run(load_questions(instances=questions))
