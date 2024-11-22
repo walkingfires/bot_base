6351e931b72059cf4db7e63cebaff6854f1fe021
walking_fires <zqop2013@gmail.com>
2024-11-23 00:30:40 +0300

    bot base clean

--- bot/users/handlers/mailing.py
+++ bot/users/handlers/mailing.py
@@ -38,39 +38,3 @@ async def mailing_to_all_users(
                     except Exception as e:
                         logger.error(f"Ошибка при отправке сообщения админу {admin_id}: {e}")
     await message.answer("Рассылка отправлена")
-
-
-# @connection(commit=False)
-# async def mailing_url_to_all_users(
-#         message: Message,
-#         command: CommandObject,
-#         session: AsyncSession,
-#         important: bool = False
-# ):
-#     users = await UserDAO.find_all_by_field(session=session, field="telegram_id")
-#     # TODO: check type
-#     mailing_text = command.args
-#     for user in users:
-#         try:
-#             await bot.send_message(
-#                 user,
-#                 mailing_text
-#             )
-#         except TelegramForbiddenError as e:
-#             logger.error(f"Пользователь {user} запретил отправку сообщений: {e}")
-#             if important:
-#                 for admin_id in admins:
-#                     try:
-#                         await bot.send_message(admin_id, f"Пользователь {user} запретил отправку сообщений")
-#                     except Exception as e:
-#                         logger.error(f"Ошибка при отправке сообщения админу {admin_id}: {e}")
-#     await message.answer("Рассылка отправлена")
-#
-#
-#
-#
-# async def cb_consultation(message: Message):
-#     await message.edit_text(
-#         "Форма обратной связи",
-#         reply_markup=consultation_keyboard()
-#     )
